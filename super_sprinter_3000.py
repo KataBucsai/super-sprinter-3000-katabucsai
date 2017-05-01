9f4dbe4380772cef2d52f938169ae808d3a9b3e7
KataBucsai <kata.kovacs@gmail.com>
2017-05-01 23:44:38 +0200

    modified py file, created story. html

--- super_sprinter_3000.py
+++ super_sprinter_3000.py
@@ -12,5 +12,19 @@ def index():
     return render_template("list.html", table=table)
 
 
+@app.route('/story')
+def story():
+    with open("story_list.csv", "r") as file:
+        lines = file.readlines()
+    table = [element.replace("\n", "").split(";") for element in lines]
+    new_story = []
+    render_template("story.html", new_story=new_story)
+    table.append(new_story)
+    with open("sroey_list.csv", "w") as file:
+        for record in table:
+            row = ';'.join(record)
+            file.write(row + "\n")
+    return render_template("list.html", table=table)
+
 if __name__ == '__main__':
     app.run(debug=True)
